70eed71c83c6d3c409692e461e809290913a2c59
Radioaktyv <65417001+Radioaktyv@users.noreply.github.com>
2020-06-14 00:23:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ ROW_COUNT = 7
 COLUMN_COUNT = 7
 MAX_RIGHT: int = 535
 MAX_LEFT: int = 85
-tablica = numpy.zeros([ROW_COUNT, COLUMN_COUNT])
-print(tablica)
+Array = numpy.zeros([ROW_COUNT, COLUMN_COUNT])
+print(Array)
 pygame.init()
 Ccenterx = 85
 Ccentery = 85
 screen = pygame.display.set_mode((620, 620))
-plansza = pygame.Rect(50, 50, 520, 520)
-wybór = pygame.Rect(50, 50, 520, 70)
+Board = pygame.Rect(50, 50, 520, 520)
+ChoiceBackground = pygame.Rect(50, 50, 520, 70)
 rysx = 10
 rysy = 10
-tura = -1
+turn = -1
 game_over = False
 xpos = 0
 myfont = pygame.font.SysFont("monospace", 50)
@@ -23,82 +23,84 @@ class Colors:
     BLACK = (0, 0, 0)
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
-def Rysuj(rysx, rysy):
-    pygame.draw.rect(screen, Colors.BLUE, plansza)
-    pygame.draw.rect(screen, (0, 150, 255), wybór)
+def Draw(rysx, rysy):
+    pygame.draw.rect(screen, Colors.BLUE, Board)
+    pygame.draw.rect(screen, (0, 150, 255), ChoiceBackground)
     for x in range(0, COLUMN_COUNT):
         rysx += 75
         rysy = 10
         for y in range(0, ROW_COUNT):
             rysy += 75
-            if tablica[x][y] == 0:
+            if Array[x][y] == 0:
                 pygame.draw.circle(screen, Colors.BLACK, (rysx, rysy), 35)
             else:
-                Krazek(rysx, rysy, tablica[x][y])
+                Krazek(rysx, rysy, Array[x][y])
             # pygame.draw.circle(screen, BLACK, (rysx, rysy), 35)
 
 
-def Krazek(Ccenterx, Ccentery, tura):
-    if tura == -1:
+def Krazek(Ccenterx, Ccentery, turn):
+    if turn == -1:
         pygame.draw.circle(screen, Colors.RED, (Ccenterx, Ccentery), 35)
-    elif tura == 1:
+    elif turn == 1:
         pygame.draw.circle(screen, Colors.YELLOW, (Ccenterx, Ccentery), 35)
 
 
-def Ruch(tablica, tura, xpos):
+def Make_A_Move(Array, turn, xpos):
     flag = True
     for y in range(6, 0, -1):
         if not flag:
             break
         ypos = y
-        if tablica[xpos][ypos] == 0:
-            tablica[xpos][ypos] = tura
+        if Array[xpos][ypos] == 0:
+            Array[xpos][ypos] = turn
             flag = False
-    nextTurn(tura)
+    nextTurn(turn)
 
 
-def nextTurn(tura):
-    if tura == -1:
-        tura += 2
+def nextTurn(turn):
+    if turn == -1:
+        turn += 2
     else:
-        tura -= 2
-    return tura
-def check(tablica,xpos,tura):
-    if tablica[xpos][1] == 0:
+        turn -= 2
+    return turn
+def check(Array, xpos, turn):
+    if Array[xpos][1] == 0:
         return True
-    if tablica[xpos][1] != 0:
+    if Array[xpos][1] != 0:
         label = myfont.render("Forbidden Move", 1, Colors.YELLOW)
         screen.blit(label, (310 - label.get_width() // 2, 310 - label.get_height() // 2))
         pygame.display.flip()
         pygame.time.wait(1000)
         return False
 
-def winning_move(tablica, tura):
+def winning_move(Array, turn):
     # Check horizontal locations for win
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
-            if tablica[r][c] == tura and tablica[r][c + 1] == tura and tablica[r][c + 2] == tura and tablica[r][c + 3] == tura:
+            if Array[r][c] == turn and Array[r][c + 1] == turn and Array[r][c + 2] == turn and Array[r][c + 3] == turn:
                 return True
 
     # Check vertical locations for win
     for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT - 4):
-            if tablica[r][c] == tura and tablica[r + 1][c] == tura and tablica[r + 2][c] == tura and tablica[r + 3][c] == tura:
+        for r in range(ROW_COUNT - 3):
+            if Array[r][c] == turn and Array[r + 1][c] == turn and Array[r + 2][c] == turn and Array[r + 3][c] == turn:
                 return True
 
     # Check positively sloped diaganols
-    for c in range(COLUMN_COUNT - 4):
-        for r in range(ROW_COUNT - 4):
-            if tablica[r][c] == tura and tablica[r + 1][c + 1] == tura and tablica[r + 2][c + 2] == tura and \
-                    tablica[r + 3][c + 3] == tura:
-                return True
+    for c in range(COLUMN_COUNT - 3):
+        if c + 3 != 0:
+            for r in range(ROW_COUNT - 3):
+                if Array[r][c] == turn and Array[r + 1][c + 1] == turn and Array[r + 2][c + 2] == turn and \
+                        Array[r + 3][c + 3] == turn:
+                    return True
 
     # Check negatively sloped diaganols
-    for c in range(COLUMN_COUNT - 4):
-        for r in range(4, ROW_COUNT):
-            if tablica[r][c] == tura and tablica[r - 1][c + 1] == tura and tablica[r - 2][c + 2] == tura and \
-                    tablica[r - 3][c + 3] == tura:
-                return True
+    for c in range(COLUMN_COUNT - 3):
+        if c + 3 != 0:
+            for r in range(3, ROW_COUNT):
+                if Array[r][c] == turn and Array[r - 1][c + 1] == turn and Array[r - 2][c + 2] == turn and \
+                        Array[r - 3][c + 3] == turn:
+                    return True
 
 
 while not game_over:
@@ -106,8 +108,10 @@ while not game_over:
     # Handle Events
 
     for event in pygame.event.get():
+        # Closing game
         if event.type == pygame.QUIT:
             sys.exit(0)
+        # Right movement
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             if Ccenterx == MAX_RIGHT:
                 Ccenterx = MAX_LEFT
@@ -115,6 +119,7 @@ while not game_over:
             else:
                 Ccenterx += 75
                 xpos += 1
+        # Left movement
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
             if Ccenterx == MAX_LEFT:
                 Ccenterx = MAX_RIGHT
@@ -122,15 +127,16 @@ while not game_over:
             else:
                 Ccenterx -= 75
                 xpos -= 1
+        # Making a move
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            if check(tablica,xpos,tura):
-                Ruch(tablica, tura, xpos)
+            if check(Array, xpos, turn):
+                Make_A_Move(Array, turn, xpos)
             else:
-                tura = tura * (-1)
-            if not winning_move(tablica, tura):
-                tura = nextTurn(tura)
+                turn = turn * (-1)
+            if not winning_move(Array, turn):
+                turn = nextTurn(turn)
             else:
-                if tura == -1:
+                if turn == -1:
                     label = myfont.render("Player Red wins!!", 1, Colors.RED)
                 else:
                     label = myfont.render("Player Yellow wins!!", 1, Colors.YELLOW)
@@ -140,7 +146,9 @@ while not game_over:
                 pygame.time.wait(3000)
 
         # drawing
-        Rysuj(rysx, rysy)
-        Krazek(Ccenterx, Ccentery, tura)
+        Draw(rysx, rysy)
+        Krazek(Ccenterx, Ccentery, turn)
         pygame.display.flip()
+        if game_over:
+            pygame.time.wait(2000)
 
